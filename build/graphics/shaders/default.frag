33f3660a491bf32b3a5df26ea017963733a2884f
Michael Ruiz <michael.ruiz@bjntech.com>
2022-11-05 13:52:39 -0500

    Added specular/ambient lighting.

--- build/graphics/shaders/default.frag
+++ build/graphics/shaders/default.frag
@@ -6,11 +6,36 @@ out vec4 FragColor;
 in vec3 color;
 // Inputs the texture coordinates from the Vertex Shader
 in vec2 texCoord;
+// Imports the normal from the Vertex Shader
+in vec3 Normal;
+// Imports the current position from the vertex shader
+in vec3 crntPos;
 
 // Gets the texture unit from the main function
 uniform sampler2D tex0;
+// Gets the color of the light from the main function
+uniform vec4 lightColor;
+// Gets the position of the light from the main function
+uniform vec3 lightPos;
+// Gets the positions of the camera from the main function
+uniform vec3 camPos;
 
 void main()
 {
-    FragColor = texture(tex0, texCoord);
+    // ambient lighting
+    float ambient = 0.20f;
+
+    // diffuse lighting
+    vec3 normal = normalize(Normal);
+    vec3 lightDirection = normalize(lightPos - crntPos);
+    float diffuse = max(dot(normal, lightDirection), 0.0f);
+
+    // specular lighting
+    float specularLight = 0.50f;
+    vec3 viewDirection = normalize(camPos - crntPos);
+    vec3 reflectionDirection = reflect(-lightDirection, normal);
+    float specAmount = pow(max(dot(viewDirection, reflectionDirection), 0.0f), 8);
+    float specular = specAmount * specularLight;
+
+    FragColor = texture(tex0, texCoord) * lightColor * (diffuse + ambient + specular);
 }
--- graphics/renderer.cpp
+++ graphics/renderer.cpp
@@ -25,23 +25,67 @@ const unsigned int height = 800;
 
 // Vertices coordinates.
 GLfloat vertices[] =
-{   //  COORDINATES      /  COLORS          /       TexCoord //
-    -0.5f, -0.0f,  0.5f,     0.83f, 0.70f, 0.44f,     0.0f, 0.0f, // Lower left corner.
-    -0.5f,  0.0f, -0.5f,     0.83f, 0.70f, 0.44f,     5.0f, 0.0f, // Lower right corner.
-     0.5f,  0.0f, -0.5f,     0.83f, 0.70f, 0.44f,     0.0f, 0.0f, // Upper corner.
-     0.5f,  0.0f,  0.5f,     0.83f, 0.70f, 0.44f,     5.0f, 0.0f, // Inner left.
-     0.0f,  0.8f,  0.0f,     0.92f, 0.86f, 0.76f,     2.5f, 5.0f  // Inner left.
+{   //  COORDINATES      /  COLORS          /       TexCoord    /    NORMALS    //
+    -0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f, 	 0.0f, 0.0f,      0.0f, -1.0f, 0.0f, // Bottom side
+    -0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	 0.0f, 5.0f,      0.0f, -1.0f, 0.0f, // Bottom side
+     0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	 5.0f, 5.0f,      0.0f, -1.0f, 0.0f, // Bottom side
+     0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	 5.0f, 0.0f,      0.0f, -1.0f, 0.0f, // Bottom side
+
+    -0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f, 	 0.0f, 0.0f,     -0.8f, 0.5f,  0.0f, // Left Side
+    -0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	 5.0f, 0.0f,     -0.8f, 0.5f,  0.0f, // Left Side
+     0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	 2.5f, 5.0f,     -0.8f, 0.5f,  0.0f, // Left Side
+
+    -0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	 5.0f, 0.0f,      0.0f, 0.5f, -0.8f, // Non-facing side
+     0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	 0.0f, 0.0f,      0.0f, 0.5f, -0.8f, // Non-facing side
+     0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	 2.5f, 5.0f,      0.0f, 0.5f, -0.8f, // Non-facing side
+
+     0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	 0.0f, 0.0f,      0.8f, 0.5f,  0.0f, // Right side
+     0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	 5.0f, 0.0f,      0.8f, 0.5f,  0.0f, // Right side
+     0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	 2.5f, 5.0f,      0.8f, 0.5f,  0.0f, // Right side
+
+     0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	 5.0f, 0.0f,      0.0f, 0.5f,  0.8f, // Facing side
+    -0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f, 	 0.0f, 0.0f,      0.0f, 0.5f,  0.8f, // Facing side
+     0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	 2.5f, 5.0f,      0.0f, 0.5f,  0.8f  // Facing side
 };
 
 GLuint indices[] =
 {
+    0, 1, 2, // Bottom side
+    0, 2, 3, // Bottom side
+    4, 6, 5, // Left side
+    7, 9, 8, // Non-facing side
+    10, 12, 11, // Right side
+    13, 15, 14 // Facing side
+};
+
+GLfloat lightVertices[] =
+{ // Coordinates
+    -0.1f, -0.1f, 0.1f,
+    -0.1f, -0.1f, -0.1f,
+    0.1f, -0.1f, -0.1f,
+    0.1f, -0.1f, 0.1f,
+    -0.1f, 0.1f, 0.1f,
+    -0.1f, 0.1f, -0.1f,
+    0.1f, 0.1f, -0.1f,
+    0.1f, 0.1f, 0.1f
+};
+
+GLuint lightIndices[] =
+{
     0, 1, 2,
     0, 2, 3,
-    0, 1, 4,
-    1, 2, 4,
-    2, 3, 4,
-    3, 0, 4
+    0, 4, 7,
+    0, 7, 3,
+    3, 7, 6,
+    3, 6, 2,
+    2, 6, 5,
+    2, 5, 1,
+    1, 5, 4,
+    1, 4, 0,
+    4, 5, 6,
+    4, 6, 7
 };
+
 
 /**
  * @brief Steps taken to render the window.
@@ -91,13 +135,46 @@ int renderer()
     EBO EBO1(indices, sizeof(indices));
 
     // Links VBO to VAO
-    VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
-    VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-    VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 11 * sizeof(float), (void*)0);
+    VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 11 * sizeof(float), (void*)(3 * sizeof(float)));
+    VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 11 * sizeof(float), (void*)(6 * sizeof(float)));
+    VAO1.LinkAttrib(VBO1, 3, 3, GL_FLOAT, 11 * sizeof(float), (void*)(8 * sizeof(float)));
     // Unbind all to prevent accidentally modifying them:
     VAO1.Unbind();
     VBO1.Unbind();
     EBO1.Unbind();
+
+    Shader lightShader("graphics/shaders/light.vert", "graphics/shaders/light.frag");
+
+    VAO lightVAO;
+    lightVAO.Bind();
+
+    VBO lightVBO(lightVertices, sizeof(lightVertices));
+    EBO lightEBO(lightIndices, sizeof(lightIndices));
+
+    lightVAO.LinkAttrib(lightVBO, 0, 3, GL_FLOAT, 3 * sizeof(float), (void*)0);
+
+    lightVAO.Unbind();
+    lightVBO.Unbind();
+    lightEBO.Unbind();
+
+    glm::vec4 lightColor = glm::vec4(1.0f, 0.0f, 1.0f, 1.0f);
+
+    glm::vec3 lightPos = glm::vec3(0.5f, 0.5f, 0.5f);
+    glm::mat4 lightModel = glm::mat4(1.0f);
+    lightModel = glm::translate(lightModel, lightPos);
+
+    glm::vec3 pyramidPos = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::mat4 pyramidModel = glm::mat4(1.0f);
+    pyramidModel = glm::translate(pyramidModel, pyramidPos);
+
+    lightShader.Activate();
+    glUniformMatrix4fv(glGetUniformLocation(lightShader.ID, "model"), 1, GL_FALSE, glm::value_ptr(lightModel));
+    glUniform4f(glGetUniformLocation(lightShader.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
+    shaderProgram.Activate();
+    glUniformMatrix4fv(glGetUniformLocation(shaderProgram.ID, "model"), 1, GL_FALSE, glm::value_ptr(pyramidModel));
+    glUniform4f(glGetUniformLocation(shaderProgram.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
+    glUniform3f(glGetUniformLocation(shaderProgram.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
 
 
     // Texture relative path.
@@ -119,11 +196,14 @@ int renderer()
         glClearColor(0.07f, 0.13f, 0.17f, 1.0f); // Color of the background.
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clean the back buffer and assign the new color to it.
 
-        // Tell OpenGL which shader program we want to use:
-        shaderProgram.Activate();
-
+        // Handles camera inputs:
         camera.Inputs(window);
-        camera.Matrix(45.0f, 0.1f, 100.0f, shaderProgram, "camMatrix");
+        // Updates and exports the camera matrix to the vertex shader:
+        camera.updateMatrix(45.0f, 0.1f, 100.0f);
+
+        shaderProgram.Activate();
+        glUniform3f(glGetUniformLocation(shaderProgram.ID, "camPos"), camera.Position.x, camera.Position.y, camera.Position.z);
+        camera.Matrix(shaderProgram, "camMatrix");
 
         // Binds texture so that it appears in rendering:
         spaceShipTexture.Bind();
@@ -131,6 +211,11 @@ int renderer()
         VAO1.Bind();
         // Draw the triangle using the GL_TRIANGLES primitive
         glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(int), GL_UNSIGNED_INT, 0);
+
+        lightShader.Activate();
+        camera.Matrix(lightShader, "camMatrix");
+        lightVAO.Bind();
+        glDrawElements(GL_TRIANGLES, sizeof(lightIndices) / sizeof(int), GL_UNSIGNED_INT, 0);
         // Sawp the back buffer with the front buffer:
         glfwSwapBuffers(window);
 
