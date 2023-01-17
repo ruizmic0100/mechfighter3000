7304d8468a246c442a29c29a29cdd5b48f7fa731
Michael <ruizmic0100@hotmail.com>
2023-01-17 17:26:01 -0600

    Adding in turn based system. (WIP)

--- game/GameEventHandler.cpp
+++ game/GameEventHandler.cpp
@@ -3,19 +3,14 @@
 
 GameState gamestate;
 
-enum BattleResults {
-    DEFEATED = 0,
-    SUCCEEDED = 1,
-    TACTICAL_RETREAT = 2
-};
-
-struct BattleData {
-    BattleResults outcome;
-} Battle;
-
 int GetGameState()
 {
     return gamestate.curr_state;
+}
+
+void SetGameState(States gamestateToSetTo)
+{
+    gamestate.curr_state = gamestateToSetTo;
 }
 
 void GameStart()
@@ -46,10 +41,12 @@ void LoadingPhase()
 
 void BattlePhase()
 {
-
     gamestate.curr_state = BATTLE_PHASE;
     // Enemy enemy = SpawnEnemy();
     renderBattleMenu = true;
+    BattleState turns;
+
+    std::cout << "Battle Sequence: " << turns.getCurrentState() << std::endl;
 }
 
 void LootPhase()
@@ -65,34 +62,4 @@ void MainMenuPhase()
 {
     gamestate.curr_state = MAINMENU_PHASE;
     renderer();
-}
-
-void PlayerWeaponAttack(Player& player, Enemy& enemy, unsigned int whichWeapon)
-{
-    // Do damage
-    // Check if kills
-    // Call loot phase
-    switch (whichWeapon) {
-        case 0: // Left arm weapon.
-            if ((enemy.enemyMech.currentAP_ -= player.playerMech.leftArmWeapon_.WeaponConfigs.AttackPower_) <= 0) {
-                enemy.enemyMech.currentAP_ = 0;
-                Battle.outcome = SUCCEEDED;
-                gamestate.curr_state = LOOT_PHASE;
-            } else {
-                enemy.enemyMech.currentAP_ -= player.playerMech.leftArmWeapon_.WeaponConfigs.AttackPower_;
-            }
-            break;
-        case 1: // Right arm weapon.
-            enemy.enemyMech.currentAP_ -= player.playerMech.rightArmWeapon_.WeaponConfigs.AttackPower_;
-            break;
-        case 2: // Left shoulder weapon.
-            enemy.enemyMech.currentAP_ -= player.playerMech.leftShoulderWeapon_.WeaponConfigs.AttackPower_;
-            break;
-        case 3: // Right shoulder weapon.
-            enemy.enemyMech.currentAP_ -= player.playerMech.rightShoulderWeapon_.WeaponConfigs.AttackPower_;
-            break;
-        default:
-            std::cout << "Could not interpret which weapon while trying to weaponattack()" << std::endl;
-            break;
-    }
 }
